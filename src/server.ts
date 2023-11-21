925eaa5cf744eec7a31541b7bd0e720be3ab89c1
NikTopler <nik.topler21@gmail.com>
2023-11-21 21:27:57 +0100

    Setup server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import helmet from "helmet";
+import cors from "cors";
+import { whiteListMiddleware } from "./middlewares";
+import { validateEnv } from "./utils";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
+
+validateEnv();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json())
+app.use(whiteListMiddleware)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
