5087908be128fcb2a6cb64fa4a01ef061f4d4253
NikTopler <nik.topler21@gmail.com>
2023-11-29 12:34:00 +0100

    Add configure files

---
+++ src/config/error-codes.ts
@@ -0,0 +1,65 @@
+export type ErrorCode = {
+    name: string;
+    code: string;
+    description: string;
+    message: string;
+}
+
+const ERROR_CODES: { [code: string]: ErrorCode } = {
+    ERR_EMPTY_FIELDS: {
+        name: 'ERR_EMPTY_FIELDS',
+        code: 'E001',
+        description: 'Empty fields',
+        message: 'Please fill out all fields'
+    },
+    ERR_INVALID_EMAIL: {
+        name: 'ERR_INVALID_EMAIL',
+        code: 'E002',
+        description: 'Invalid email',
+        message: 'Please enter a valid email'
+    },
+    ERR_INVALID_PASSWORD: {
+        name: 'ERR_INVALID_PASSWORD',
+        code: 'E003',
+        description: 'Invalid password',
+        message: 'Please enter a valid password'
+    },
+    ERR_INVALID_PHONE_NUMBER: {
+        name: 'ERR_INVALID_PHONE_NUMBER',
+        code: 'E004',
+        description: 'Invalid phone number',
+        message: 'Please enter a valid phone number'
+    },
+    ERR_INVALID_FIRST_NAME: {
+        name: 'ERR_INVALID_FIRST_NAME',
+        code: 'E005',
+        description: 'Invalid first name',
+        message: 'Please enter a valid first name'
+    },
+    ERR_INVALID_LAST_NAME: {
+        name: 'ERR_INVALID_LAST_NAME',
+        code: 'E006',
+        description: 'Invalid last name',
+        message: 'Please enter a valid last name'
+    },
+    ERR_EMAIL_ALREADY_EXISTS: {
+        name: 'ERR_EMAIL_ALREADY_EXISTS',
+        code: 'E007',
+        description: 'Email already exists',
+        message: 'This email already exists'
+    },
+    ERR_COULD_NOT_SIGN_UP: {
+        name: 'ERR_COULD_NOT_SIGN_UP',
+        code: 'E008',
+        description: 'Could not sign up',
+        message: 'Could not sign up. Please try again later.'
+    },
+    ERR_SOMETHING_WENT_WRONG: {
+        name: 'ERR_SOMETHING_WENT_WRONG',
+        code: 'E009',
+        description: 'Something went wrong',
+        message: 'Something went wrong. Please try again later.'
+    }
+};
+
+export default ERROR_CODES;
---
+++ src/modules/shared/utils/index.ts
@@ -0,0 +1,5 @@
+import validateEnv from "./validateEnv";
+
+export {
+    validateEnv
+}
